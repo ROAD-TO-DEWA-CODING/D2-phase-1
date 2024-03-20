eaf3e2e3a76f7b6c606d046eb3b4ea25a75fb951
unknown <mauritius.william.wijaya1@gmail.com>
2024-03-20 15:19:33 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,79 @@
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
+//? ITERABLE
+// suatu yang dapat di loop
+const city = 'Jakarta'
 
+//* bracket notation
+
+
+
+
+//* length
+
+
+
+
+//? ARRAY
+const numbers = [3, 2, 1, 6, 4, 9, 1, 3]
+
+
+// type data in array could be anything
+
+//* push
+// numbers.push(0)
+// console.log(numbers);
+
+//* pop
+// numbers.pop()
+// console.log(numbers);
+
+//* unshift
+// numbers.unshift(9)
+// console.log(numbers);
+
+//* shift
+// numbers.shift()
+// console.log(numbers);
+
+
+
+//TODO : find the biggest number
+
+
+
+
+//TODO : find the smallest
+
+
+
+
+//TODO : find the longest length
+const personsName = ['John Cena', 'Christiano Ronaldo', 'Joko Widodo', 'Cetar Membahana']
+
+
+
+
+//TODO : Sum all the numbers
+
+
+
+
+//TODO : reverse string
+let reverse
+
+
+
+
+//TODO : mencari palindrome
+const words = ['katak', 'bapak', 'radar', 'repaper', 'issue']
+
+
+
+
+
+//TODO : find the biggest number
+const nestedArray = [18, 21, 56, 121, [122, 22, 66], 11, 6, [35, 82, 12, 29]]
+
+console.log(nestedArray[0].length);
+// for (let i = 0; i < nestedArray.length; i++) {
+//     if (nestedArray.length)    
+// }
---
+++ pr.js
@@ -0,0 +1,9 @@
+//TODO : 1. merge and sorting this number
+let numbers1 = Array.from({length: 10}, () => Math.ceil(Math.random() * 100))
+let numbers2 = Array.from({length: 10}, () => Math.ceil(Math.random() * 100))
+
+// the result must be one array : [...sorted number]
+
+
+
+//TODO : 2. merge and sorting this number    
